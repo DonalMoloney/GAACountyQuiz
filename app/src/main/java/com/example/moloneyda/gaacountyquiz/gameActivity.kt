2775a6d3fcdc5bd474f32aad43c7479812a66d7d
donalmshs@gmail.com <Manila1998>
2018-11-27 10:08:56 -0600

    Added skeleton structure of gameActivity.kt

--- app/src/main/java/com/example/moloneyda/gaacountyquiz/gameActivity.kt
+++ app/src/main/java/com/example/moloneyda/gaacountyquiz/gameActivity.kt
@@ -18,15 +18,15 @@ class gameActivity : AppCompatActivity() {
         val quitBtn = findViewById<Button>(R.id.btnQuitA2)
         imageViewGameA2.setImageResource(R.drawable.home)
         enterBtn.setOnClickListener {
-            val cards = arrayOf(R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1,
-                    R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1,
-                    R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1,
-                    R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1,
-                    R.drawable.p1,R.drawable.p1, R.drawable.p1,R.drawable.p1,R.drawable.p1,
-                    R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1
-                    ,R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1,R.drawable.p1)
-            // val imageView = findViewById<ImageView>(R.id.imageViewGameA2)
-            // val currentImage = "C:/Users/Moloneyda/AndroidStudioProjects/GaaApp/app/src/main/res/home.jpg"
+            val cards = arrayOf(R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1,
+                    R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1,
+                    R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1,
+                    R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1,
+                    R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1,
+                    R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1
+                    , R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1, R.drawable.p1)
+            val currentImage = getImage()
+            val name = getCorrectName(currentImage)
             //imageView.
 
 
@@ -47,4 +47,116 @@ class gameActivity : AppCompatActivity() {
         //Display toast telling the user you are ready
         Toast.makeText(this, "Get Ready to play", Toast.LENGTH_SHORT).show()
     }
+
+    /**
+     * Application gets the correct name of the county based on the image name passed in
+     * Todo method still needs to be implemented
+     */
+    private fun getCorrectName(image: Any): String {
+        val imageName = image.toString()
+        if (imageName.equals("p1.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p1.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p2.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p3.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p4.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p5.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p6.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p7.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p8.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p9.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p10.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p11.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p12.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p13.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p14.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p15.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p16.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p17.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p18.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p19.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p20.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p21.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p22.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p23.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p24.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p25.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p26.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p27.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p28.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p29.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p30.jpg")) {
+            return ""
+
+        } else if (imageName.equals("p31.jpg")) {
+            return ""
+
+        } else {
+            return ""
+        }
+    }
+
+    private fun getImage(): Any {
+
+    }
+
 }
