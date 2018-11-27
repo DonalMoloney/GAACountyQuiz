c099f26c6f705ea37dba9ed4fac50a44fe404c5d
donalmshs@gmail.com <Manila1998>
2018-11-27 09:46:40 -0600

    Implemented the home.kt file, added handlers to the play and quit button

--- app/src/main/java/com/example/moloneyda/gaacountyquiz/home.kt
+++ app/src/main/java/com/example/moloneyda/gaacountyquiz/home.kt
@@ -1,12 +1,27 @@
 package com.example.moloneyda.gaacountyquiz
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val playBtn = findViewById<Button>(R.id.playbtnM)
+        val quitBtn = findViewById<Button>(R.id.quitbtnM)
+
+        playBtn.setOnClickListener {
+            val intent = Intent(this, gameActivity::class.java)
+            startActivity(intent)
+        }
+
+        quitBtn.setOnClickListener {
+            finish()
+            System.exit(0)
+        }
     }
 }
