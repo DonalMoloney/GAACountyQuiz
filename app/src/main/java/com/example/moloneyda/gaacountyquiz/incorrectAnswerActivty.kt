c903897602ac211860f9ddfe7d920efbfe2e0acb
donalmshs@gmail.com <Manila1998>
2018-11-27 10:27:50 -0600

    Added incorrecAnswerActivity.kt to display the correct answer if user input was incorrect. Removed the first one

---
+++ app/src/main/java/com/example/moloneyda/gaacountyquiz/incorrectAnswerActivty.kt
@@ -0,0 +1,13 @@
+package com.example.moloneyda.gaacountyquiz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class incorrectAnswerActivty : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_correct_answer_activty)
+    }
+}
+
